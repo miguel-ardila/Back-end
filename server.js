c26b112f410de830a73233ff7eb4a24abc1d4a37
Miguel Ardila <miguel_sxe@hotmail.com>
2023-09-08 22:15:25 -0500

    else if linea 16

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const port = 3000; //puerto
 //req = solicitudes
 //res = resouestas
 const server = http.createServer((req, res) => {
+    const ruta = new URL(req.url,`http://${host}:${port}`);
+    console.log(ruta);
+
+    if (ruta.pathname === "/persona"){
+        res.writeHead(200);
+        res.write(JSON.stringify({nombre: ruta. "/persona"}));
+    }else if (ruta.pathname === "/user"){
+        res.writeHead(200);
+        res.write(JSON.stringify("/user"));
+    }
+
+
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello World!\n");
 });
